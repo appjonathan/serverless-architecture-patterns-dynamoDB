da1601b3be607466785d0c91c9bc6be23872efea
eppjonathan <epp.j.epp@googlemail.com>
2024-01-16 12:39:47 +0100

    main.tf updated

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.23.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
+
 provider "aws" {
   region = var.region
 }
